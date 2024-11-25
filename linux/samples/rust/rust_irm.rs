fca298a9d962e6fb7ebbda8055522c322f01383e
Began <beganbajrami@gmail.com>
2024-11-25 16:12:08 +0100

    ok

---
+++ linux/samples/rust/rust_irm.rs
@@ -0,0 +1,26 @@
+//! Innocent Rusty Module
+
+use kernel::prelude::*;
+
+module!{
+    type: IRM,
+    name: "irm",
+    license: "GPL",
+}
+
+
+struct IRM;
+
+impl file::Operations for IRM {
+    fn open(context: &Self::OpenData, file: &file::File) -> Result<Self::Data> {
+        pr_info!("File was opened\n");
+        Ok(())
+    }
+}
+
+impl kernel::Module for IRM {
+    fn init(_module: &'static ThisModule) -> kernel::error::Result<Self> {
+        pr_info!("Hello World\n");
+        Ok(IRM)
+    }
+}
